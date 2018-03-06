507238da900bfc5d84dc5a8a4a13c6b755bb700c
Mark Featherston <mark@embeddedarm.com>
2018-03-06 12:17:05 -0700

    ts7840: Enable ECC by default

--- board/technologic/ts7840/ts7840.c
+++ board/technologic/ts7840/ts7840.c
@@ -70,7 +70,7 @@ static struct mv_ddr_topology_map ts7840_ecc_topology_map = {
 	    DDR_FREQ_800,		/* frequency */
 	    0, 0,			/* cas_l cas_wl */
 	    MV_DDR_TEMP_LOW} },		/* temperature */
-	BUS_MASK_32BIT,		/* subphys mask */
+	BUS_MASK_32BIT_ECC,		/* subphys mask */
 	MV_DDR_CFG_DEFAULT,		/* ddr configuration data source */
 	{ {0} },			/* raw spd data */
 	{0}				/* timing parameters */
