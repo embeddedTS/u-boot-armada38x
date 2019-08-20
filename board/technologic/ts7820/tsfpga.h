c9f8198988ef7ababeed3acb63270aa6886752cd
Mark Featherston <mark@embeddedarm.com>
2019-08-20 10:24:27 -0700

    ts7820: Updated FPGA IO registers for latest board revision

--- board/technologic/ts7820/tsfpga.h
+++ board/technologic/ts7820/tsfpga.h
@@ -4,11 +4,14 @@
 #include <asm/arch/cpu.h>
 #include <asm/io.h>
 
-#define TS7820_SYSCON_BASE   (MBUS_PCI_MEM_BASE)
-#define TS7820_SYSCON_SIZE   0x48
+#define TS_FPGA_VENDOR		0x1e6d
+#define TS7820_FPGA_DEVICE	0x7820
+#define TS7840_FPGA_DEVICE	0x7840
 
-#define TS7820_FPGA_VENDOR   0x1172
-#define TS7820_FPGA_DEVICE   0x0004
+#define FPGA_MODEL		0x0
+#define FPGA_ASMI		0x8
+#define FPGA_FRAM_BOOTCOUNT	0x18
+#define FPGA_RELOAD		0x1a
 
 extern void __iomem *syscon_base;
 
@@ -16,20 +19,27 @@ extern void __iomem *syscon_base;
 #define fpga_poke32(offset, value) writel(value, syscon_base + (offset))
 
 /* Output Enables */
-#define fpga_dio_oe_set(bank, value) fpga_poke32(0, value)
-#define fpga_dio_oe_clr(bank, value) fpga_poke32(0 + 0x4, value)
+#define fpga_dio_oe_set(bank, value) \
+	fpga_poke32(0x40 + (bank * 0x1c) + 0x00, value)
+#define fpga_dio_oe_clr(bank, value) \
+	fpga_poke32(0x40 + (bank * 0x1c) + 0x04, value)
 
 /* Output Data */
-#define fpga_dio_dat_set(bank, value) fpga_poke32(0 + 0x8, value)
-#define fpga_dio_dat_clr(bank, value) fpga_poke32(0 + 0xc, value)
+#define fpga_dio_dat_set(bank, value) \
+	fpga_poke32(0x40 + (bank * 0x1c) + 0x08, value)
+#define fpga_dio_dat_clr(bank, value) \
+	fpga_poke32(0x40 + (bank * 0x1c) + 0x0c, value)
 
 /* Output Data Readback */
-#define fpga_dio_dat_out(bank) fpga_peek32(0 + 0x8)
+#define fpga_dio_dat_out(bank) \
+	fpga_peek32(0x40 + (bank * 0x1c) + 0x08)
 
 /* Output Enable Data Readback */
-#define fpga_dio_oe(bank) fpga_peek32(0 + 0x0)
+#define fpga_dio_oe(bank) \
+	fpga_peek32(0x40 + (bank * 0x1c) + 0x00)
 
 /* Input Data */
-#define fpga_dio_dat_in(bank) fpga_peek32(0 + 0xc)
+#define fpga_dio_dat_in(bank) \
+	fpga_peek32(0x40 + (bank * 0x1c) + 0x0c)
 
 #endif
